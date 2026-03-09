b090f021900599d0c7813a5c25d9712227ac1ede
ST10528326 <ST10528326@myemeris.edu.za>
2026-03-09 11:43:54 +0200

    Input & Output
    - New method to receive user input and create a message to return
    
    - Main method to run  input method and display results.

--- MyFirstGithubProject/src/main/java/com/mycompany/myfirstgithubproject/MyFirstGithubProject.java
+++ MyFirstGithubProject/src/main/java/com/mycompany/myfirstgithubproject/MyFirstGithubProject.java
@@ -1,8 +1,26 @@
 package com.mycompany.myfirstgithubproject;
 
+import java.util.Scanner;
+
 public class MyFirstGithubProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String display = "\nHey " + userInput() + ", lock in fn!";
+        System.out.println(display);
+        
+    }
+    
+    public static String userInput(){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter name: ");
+        String name = input.nextLine();
+        
+        System.out.println("Enter surname: ");
+        String surname = input.nextLine();
+        
+        String fullName = name + " " + surname;
+        
+        return fullName;
     }
 }
